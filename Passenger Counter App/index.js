ee11abbab17fcaccb2620c575a7acd24f65fc09e
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 10:54:45 +0530

    Document Object Model

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -1,12 +1,4 @@
-
-// change the count-el in the html to reflect the new count
-
-// camelCase
 let countEl = document.getElementById("count-el") 
-// pass in arguements
-
-console.log(countEl)
-
 let count = 0
 
 function increment(){

d9e61114e9cf70a74b7d70d8e5c91e574b821888
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 18:38:16 +0530

    if/else...if/else

--- Build Blackjack Game/index.js
+++ Build Blackjack Game/index.js
@@ -1,12 +1,12 @@
 // Check if the person is old enough to enter the nightclub (21)
 // Log a suitable message to the console in both cases
 
-let age = 22
+let age = 21
 
 // If less than 21 -> "You can not enter the club!"
-if (age <= 21) {
+if (age < 21) {
     console.log("You can not enter the club!")
-} else if (age ===21) {
+} else if (age == 21) {
     console.log("You are welcome!")
 }
 else {

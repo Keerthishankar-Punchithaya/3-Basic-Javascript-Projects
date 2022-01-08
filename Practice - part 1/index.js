aa2612da156e02001b570a2538e13add4cec6773
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 14:22:39 +0530

    Strings and numbers

--- Practice - part 1/index.js
+++ Practice - part 1/index.js
@@ -1,22 +1,7 @@
-let myPoints = 3
-
-// Create two functions, add3Points() and removePoints(), and have them
-// add/remove points to/from the myPoints variable
-
-function add3Points() {
-    myPoints += 3
-}
-
-function removePoints() {
-    myPoints -= 1
-}
-
-add3Points()
-add3Points()
-add3Points()
-removePoints()
-removePoints()
-
-
-// Call the functions to that the line below logs out 10
-console.log(myPoints)
+// Try to predict what each of the lines will log out
+console.log("2" + 2)
+console.log(11 + 7)
+console.log(6 + "5")
+console.log("My points: " + 5 + 9)
+console.log(2 + 2)
+console.log("11" + "14")

4539a6f04d09c5e984c27808a1bc681a9e7b2a14
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 08:43:40 +0530

    Using functions to write less code

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -1,13 +1,17 @@
-// document.getElementById("count-el").innerText = 5
+// Setting up the race 🏎️🏎️🏎️
 
-// let count = 5
-
-
-// initialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-
-function increment() {
-    console.log("The button was clicked")
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
 }
+
+countdown()
+
+// GO! 🏁
+// Players are running the race 🏎️💨
+// Race is finished!
+
+// Get ready for a new race

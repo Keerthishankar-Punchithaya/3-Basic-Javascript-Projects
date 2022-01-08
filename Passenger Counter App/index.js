7fa74d91f2a75e2e4a60e7718e5e61df85f1bf95
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 08:33:38 +0530

    The onclick event listener

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -2,45 +2,12 @@
 
 // let count = 5
 
-// count = count + 1
-// count = count + 2
-// count = count + 10
-// console.log(count)
 
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-// let bonusPoints = 50
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints + 50
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints - 75
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints + 45
-// console.log(bonusPoints)
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(count)
-
-
-// 1. Create a variable, myAge, and set its value to your age
-// let myAge = 20
-
-// 2. Log the myAge variable to the console
-// console.log(myAge)
-
-
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge
-// 3. Log myDogAge to the console 
+function increment() {
+    console.log("The button was clicked")
+}

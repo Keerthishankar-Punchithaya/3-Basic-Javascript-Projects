d3eecfaef180f3e699146477fe63f162e67033f2
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 10:44:23 +0530

    Document the Object Model

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -1,13 +1,17 @@
-// initialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked (log it out)
+
 // change the count-el in the html to reflect the new count
+
+// camelCase
+let countEl = document.getElementById("count-el") 
+// pass in arguements
+
+console.log(countEl)
 
 let count = 0
 
 function increment(){
     count = count + 1
-    console.log(count)
+    countEl.innerText = count
 }
 
 increment()

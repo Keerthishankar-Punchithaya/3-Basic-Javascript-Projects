a209fb60c80e3fd887168832b4718ba87ebe1808
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 14:18:08 +0530

    Incrementing and decrementing

--- Practice - part 1/index.js
+++ Practice - part 1/index.js
@@ -1,8 +1,22 @@
-let name = "Keerthishankar"
-let greeting = "Hi there"
+let myPoints = 3
 
-function greet() {
-    console.log(greeting + ", " + name + "!")
+// Create two functions, add3Points() and removePoints(), and have them
+// add/remove points to/from the myPoints variable
+
+function add3Points() {
+    myPoints += 3
 }
 
-greet()
+function removePoints() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+removePoints()
+removePoints()
+
+
+// Call the functions to that the line below logs out 10
+console.log(myPoints)

cfc4c37c8bb9f94cbb1c75f9141281efcadeeff2
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 11:51:49 +0530

    Use plus equal for count

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -1,17 +1,12 @@
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
-let welcomeEl = document.getElementById("welcome-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
-// Create two variables (name & greeting) that contains your name
-// and the greeting we want to render on the page
+function increment() {
+    // Change this to use the plus equal technique you've learned
+    count += 1 
+    countEl.innerText = count
+}
 
-let name = "Keerthishankar"
-let greeting = "Welcome back "
-
-// Render the welcome message using welcomeEl.innerText
-welcomeEl.innerText = greeting + name
-
-// Add an emoji to the end! 👋
-// WRITE YOUR CODE BELOW HERE
-// HINT: count = count + 1
-
-welcomeEl.innerText += "👋"
+function save() {
+    console.log(count)
+}

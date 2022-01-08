3e755fd737939b64b7cb7e0119455b5e9ed6e916
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 11:04:27 +0530

    Create the save button

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -6,4 +6,10 @@ function increment(){
     countEl.innerText = count
 }
 
-increment()
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save() {
+    console.log(count)
+}
+

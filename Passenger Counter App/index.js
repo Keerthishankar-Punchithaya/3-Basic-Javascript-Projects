322d003fa03fee52614b8b0ecf0a92cb3440f63c
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 10:26:42 +0530

    write a function that logs the sum

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -1,8 +1,11 @@
-// Create a function (you decide the name) that logs out the number 42 to the console
-// Call/invoke the function
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
 
-function myLogger() {
-    console.log(42)
+// Create a function that logs out the sum of all the lap times
+function logLapTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
 }
 
-myLogger()
+logLapTime()

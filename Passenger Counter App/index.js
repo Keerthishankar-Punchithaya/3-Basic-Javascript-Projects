9cc0ad9feb3cd1190ed35514beb43329f2e4c005
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 10:19:48 +0530

    write your first function

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -1,17 +1,8 @@
-// Setting up the race 🏎️🏎️🏎️
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
 
-function countdown() {
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
+function myLogger() {
+    console.log(42)
 }
 
-countdown()
-
-// GO! 🏁
-// Players are running the race 🏎️💨
-// Race is finished!
-
-// Get ready for a new race
+myLogger

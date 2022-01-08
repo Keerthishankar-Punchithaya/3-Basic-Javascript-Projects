098579ec8f3b1bc81cbe4897e19184b9a4f0e805
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 11:21:56 +0530

    Log a greeting to the console

--- Passenger Counter App/index.js
+++ Passenger Counter App/index.js
@@ -1,11 +1,12 @@
-let username = "per"
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
 
-// Create a variable , message, that stores the string: "You have three new notifications"
+// Create a third variable, myGreeting, that concatenates the two strings
+// Log myGreeting to the console
 
-let message = "You have three new notifications"
+let name = "Keerthishankar"
+let greeting = "Hi, my name is "
 
-let messageToUser = message + ", " + username + "!"
+let myGreeting = greeting + name
 
-console.log(messageToUser)
-
-// Create a variable, messageToUser, that contains the messagewe have logged
+console.log(myGreeting)

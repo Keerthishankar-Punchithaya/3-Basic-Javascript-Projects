3c983b1d419a7369451ec075f740938793abd5e8
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-08 07:09:49 +0530

    initial commit

---
+++ Passenger Counter App/index.js
@@ -0,0 +1,46 @@
+// document.getElementById("count-el").innerText = 5
+
+// let count = 5
+
+// count = count + 1
+// count = count + 2
+// count = count + 10
+// console.log(count)
+
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(count)
+
+
+// 1. Create a variable, myAge, and set its value to your age
+// let myAge = 20
+
+// 2. Log the myAge variable to the console
+// console.log(myAge)
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console 

81c0f0914edbbcd0b17b8ae55a48ae3f0c880b1f
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-09 16:03:15 +0530

    Intro to Arrays

---
+++ Build Blackjack Game/intro to arrays/array.js
@@ -0,0 +1,9 @@
+// Arrays - ordered list of items
+
+let featuredPosts = [
+    "Check out my netflix clone", 
+    "Here's the code for my project", 
+    "I've relaunched "
+]
+
+let exp = ["123", "abc", "456"]

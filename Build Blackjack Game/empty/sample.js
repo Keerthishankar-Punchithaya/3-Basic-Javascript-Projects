c0b6e64675f4d5b0899085cf736aef5ebfb87df6
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-10 05:20:07 +0530

    Create your first object

--- Build Blackjack Game/empty/sample.js
+++ Build Blackjack Game/empty/sample.js
@@ -1,12 +1,10 @@
-let course = {
-    title: "qwerty",
-    lessons: 14,
-    creator: "creator",
-    length: 63,
-    level: 2,
-    isFree: true,
-    tags: ["html","css"]
+let castle = {
+    title: "abc",
+    price: 190,
+    isSuperHost: true,
+    images: ["https://images.unsplash.com/photo-1641203423173-7a86a982533d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+    "https://images.unsplash.com/photo-1641391503184-a2131018701b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"]
 }
 
-console.log( course["tags"] )
-
+console.log(castle.price)
+console.log(castle.isSuperHost)

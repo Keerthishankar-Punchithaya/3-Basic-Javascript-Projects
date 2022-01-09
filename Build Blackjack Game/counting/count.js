d9ee10e9ae41c771aae25ac9b732f70324db6df3
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-09 16:42:17 +0530

    Write your first array-based loop

--- Build Blackjack Game/counting/count.js
+++ Build Blackjack Game/counting/count.js
@@ -1,4 +1,5 @@
-let a = ["1","2","3","4","5","6"]
-for (let i = 0; i < 6; i += 1) {
-    console.log(a[i])
+let cards = [4, 5, 6]
+
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i])
 }

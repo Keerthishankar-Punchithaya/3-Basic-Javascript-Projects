b5642ef69abd1a41ed39ef8894394f8203dd9058
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-09 11:16:59 +0530

    Add the hasBlackJack varialbe

--- Build Blackjack Game/index.js
+++ Build Blackjack Game/index.js
@@ -1,19 +1,22 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
 // Write the conditional according to these rules:
 
 // if less than or equal to 20 -> "Do you want to draw a new card?"
 // else if exactly 21 -> "Wohoo! You've got Blackjack!"
 // else -> "You're out of the game!"
-
-
-
 if (sum <= 20) {
     console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack!")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game!")
 }
+
+
+// CASH OUT!
+console.log(hasBlackJack)

9a2584561a6b9f1f228b820f9e7e063507b7bed9
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-09 14:04:17 +0530

    Make the start button work

--- Build Blackjack Game/index.js
+++ Build Blackjack Game/index.js
@@ -6,17 +6,23 @@ let isAlive = true
 // 1. Declare a variable called message and assign its value to an empty string 
 let message = ""
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+// Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    // Log it out!
+    console.log(message)
 }
 
 
-// Log it out!
-console.log(message)
+
+

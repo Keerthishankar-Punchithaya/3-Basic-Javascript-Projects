8f5b639e95d20847be6eb4c1b83bb0f117c1021f
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-09 19:41:56 +0530

    Complete getRandomNumber function

--- Build Blackjack Game/index.js
+++ Build Blackjack Game/index.js
@@ -11,7 +11,16 @@ let cardsEl = document.getElementById("cards-el")
 
 // Make this function return a random number between 1 and 13
 function getRandomCard() {
-    return Math.floor( Math.random()*13 ) + 1 
+    // if 1      -> return 11
+    // if 11-13  -> return 10
+    let randomNumber = Math.floor( Math.random()*13 ) + 1 
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
 function startGame() {

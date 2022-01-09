a66cbb0aa07ec4aa1b3f51f704672a03393e026b
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-09 16:19:09 +0530

    Adding and removing items from arrays

--- Build Blackjack Game/intro to arrays/array.js
+++ Build Blackjack Game/intro to arrays/array.js
@@ -1,1 +1,12 @@
-let per = ["123", "456", true]
+let message = [
+    "hello",
+    "123",
+    "456"
+]
+console.log(message)
+let newmessage = "789"
+
+messages.push(newmessage)
+
+console.log(message)
+console.log(newmessage)

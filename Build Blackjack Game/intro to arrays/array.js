5f4bcaa6e9422c035d82e1388db7e9127f59bfa9
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-09 16:11:56 +0530

    Arrays with multiple datatypes

--- Build Blackjack Game/intro to arrays/array.js
+++ Build Blackjack Game/intro to arrays/array.js
@@ -1,12 +1,1 @@
-// Arrays - ordered list of items
-
-let featuredPosts = [
-    "Check out my netflix clone", 
-    "Here's the code for my project", 
-    "I've relaunched "
-]
-
-console.log(featuredPosts[0])
-console.log(featuredPosts.length)
-
-let exp = ["123", "abc", "456"]
+let per = ["123", "456", true]

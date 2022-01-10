8e1905d7d37ba5a9bfb722eaa8b4e3a03b19dbcb
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-10 06:46:42 +0530

    if else

--- Practice - part 2/index.js
+++ Practice - part 2/index.js
@@ -1,11 +1,13 @@
-let person = {
-    name: "Keerthishankar",
-    age: 21,
-    country: "India"
-}
+let age = 18
 
-function logData() {
-    console.log(person.name + " " + "is " + person.age + " " + "years old and lives in" + " " + person.country )
+if (age < 6) {
+    console.log("free")
+} else if (age < 18) {
+    console.log("child discount")
+} else if (age < 27) {
+    console.log("student discount")
+} else if (age < 67) {
+    console.log("full price")
+} else {
+    console.log("senior citizen discount")
 }
-
-logData()

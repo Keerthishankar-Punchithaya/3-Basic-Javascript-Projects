04e59b3cadb85028ffbe290d1c095d46a2319e17
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-10 05:49:56 +0530

    Use an object to store player data

--- Build Blackjack Game/index.js
+++ Build Blackjack Game/index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Keerthishankar",
+    chips: 200
+}
 let cards = [] //array - ordered list of items
 let sum = 0
 let hasBlackJack = false
@@ -7,13 +11,8 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-let player = {
-    name: "Keerthishankar"
-    chips: 145
-}
-
-
 let playerEl = document.getElementById("player-el")
+
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {

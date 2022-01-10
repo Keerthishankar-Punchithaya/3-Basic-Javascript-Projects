9ee53542a946d8d365550cc40ab94e61eb9bd731
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-10 06:40:04 +0530

    Objects and functions

---
+++ Practice - part 2/index.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Keerthishankar",
+    age: 21,
+    country: "India"
+}
+
+function logData() {
+    console.log(person.name + " " + "is " + person.age + " " + "years old and lives in" + " " + person.country )
+}
+
+logData()

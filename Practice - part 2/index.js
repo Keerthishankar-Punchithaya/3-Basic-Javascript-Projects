2d5df37ea7108cc85f244b456d5a59c3d145d9ca
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-10 06:48:57 +0530

    Loops and arrays

--- Practice - part 2/index.js
+++ Practice - part 2/index.js
@@ -1,13 +1,5 @@
-let age = 18
+let array = ["123","456","789"]
 
-if (age < 6) {
-    console.log("free")
-} else if (age < 18) {
-    console.log("child discount")
-} else if (age < 27) {
-    console.log("student discount")
-} else if (age < 67) {
-    console.log("full price")
-} else {
-    console.log("senior citizen discount")
+for ( let i = 0; i < array.length; i++) {
+    console.log(array[i])
 }

aa7ba2ec8472f8036055614b5274d88d28c68e96
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-13 06:41:17 +0530

    Create the myLeads array and inputEl

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -1,9 +1,12 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
-// inputBtn.addEventListener("click", function() {
-//     console.log("Input Button clicked!")
-// })
 
-function saveLead() {
+inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
-}
+})
+
+// function saveLead() {
+//     console.log("Button clicked!")
+// }

23e413e162dd0380676ff0be01398e730c0511bf
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-13 06:26:43 +0530

    Write your first addEventListener

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -1,9 +1,5 @@
-function saveLead() {
-    console.log("Button clicked from onclick attribute")
-}
+let box = document.getElementById("box")
 
-let inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
+box.addEventListener("click", function() {
+    console.log("I want to open the box!")
 })

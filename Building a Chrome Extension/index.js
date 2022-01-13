7cbbec2bbe47f9dab8cead8b1aa0faedfa0afedc
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-13 07:04:13 +0530

    Create the unordered list

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -2,6 +2,10 @@ let myLeads = ["www.lead1.com","www.lead2.com","www.lead3.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
+const ulEl = document.getElementById("ul-el")
+
+console.log(ulEl)
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)

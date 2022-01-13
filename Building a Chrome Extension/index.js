8f7ab2b0d19804bc5276f9c747ce5bb128641959
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-13 14:05:51 +0530

    Render the leads in the unordered list(notworking)

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -1,10 +1,8 @@
-let myLeads = ["www.lead1.com","www.lead2.com","www.lead3.com"]
+let myLeads = ["www.lead1.com","www.lead2.com","www.lead3.com","www.lead4.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
 const ulEl = document.getElementById("ul-el")
 
-console.log(ulEl)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
@@ -13,5 +11,5 @@ inputBtn.addEventListener("click", function() {
 
 
 for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+    ulEl.textContent += myLeads[i] + " "
 }

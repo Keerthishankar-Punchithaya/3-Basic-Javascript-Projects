7c8fcc50c8e8560a20167862654144a74be3d13c
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-14 11:12:53 +0530

    Render the <li> elements with innerHTML

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -1,25 +1,25 @@
-// let myLeads = ["www.lead1.com","www.lead2.com","www.lead3.com","www.lead4.com"]
-// const inputEl = document.getElementById("input-el")
-// const inputBtn = document.getElementById("input-btn")
-// const ulEl = document.getElementById("ul-el")
+let myLeads = ["www.lead1.com","www.lead2.com","www.lead3.com","www.lead4.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 
-// inputBtn.addEventListener("click", function() {
-//     myLeads.push(inputEl.value)
-//     console.log(myLeads)
-// })
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
 
 
-// for (let i = 0; i < myLeads.length; i++) {
-//     ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-// }
-
-
-
-const container = document.getElementById("container")
-
-container.innerHTML = "<button onclick='buy()'>Buy!</button>"
-
-function buy() {
-    container.innerHTML += "<p>Thank you for buying!</p>"
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 }
+
+
+
+// const container = document.getElementById("container")
+
+// container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+// function buy() {
+//     container.innerHTML += "<p>Thank you for buying!</p>"
+// }

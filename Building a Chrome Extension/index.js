200863796535b9a4f460edef208e4bfb6cd13ca1
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-14 11:07:09 +0530

    Write your first innerHTML

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -1,15 +1,21 @@
-let myLeads = ["www.lead1.com","www.lead2.com","www.lead3.com","www.lead4.com"]
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+// let myLeads = ["www.lead1.com","www.lead2.com","www.lead3.com","www.lead4.com"]
+// const inputEl = document.getElementById("input-el")
+// const inputBtn = document.getElementById("input-btn")
+// const ulEl = document.getElementById("ul-el")
 
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
-})
+// inputBtn.addEventListener("click", function() {
+//     myLeads.push(inputEl.value)
+//     console.log(myLeads)
+// })
 
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-}
+// for (let i = 0; i < myLeads.length; i++) {
+//     ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+// }
+
+
+
+const container = document.getElementById("container")
+
+container.innerHTML = "<button>Buy!</button>"

516c2afea28cd9563159b8debd7d406765a52660
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-14 11:10:33 +0530

    More innerHTML practice

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -18,4 +18,8 @@
 
 const container = document.getElementById("container")
 
-container.innerHTML = "<button>Buy!</button>"
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+function buy() {
+    container.innerHTML += "<p>Thank you for buying!</p>"
+}

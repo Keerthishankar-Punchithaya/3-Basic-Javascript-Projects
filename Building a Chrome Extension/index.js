2707433daa085d84d153af63f9f0f786952e6fc6
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-15 07:01:47 +0530

    Create the render function

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.lead1.com","www.lead2.com","www.lead3.com","www.lead4.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -6,25 +6,13 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 })
 
-let listItems = ""
-
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
-
-
-// const container = document.getElementById("container")
-
-// container.innerHTML = "<button onclick='buy()'>Buy!</button>"
-
-// function buy() {
-//     container.innerHTML += "<p>Thank you for buying!</p>"
-// }

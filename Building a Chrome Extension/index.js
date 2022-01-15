9dabd1e5d0fbf96e94986f0106d1feeb66a4e5f5
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-15 07:12:45 +0530

    Add the <a> tag

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -13,7 +13,8 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        ulEl.innerHTML += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        console.log(listItems)
     }
     ulEl.innerHTML = listItems
 }

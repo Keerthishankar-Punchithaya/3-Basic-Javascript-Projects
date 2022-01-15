0bb623d6e38b96cb3bbf9c205fda3ed76b0d9fa2
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-15 09:45:19 +0530

    your first localStorage

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -3,6 +3,12 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// localStorage.setItem("myName", "Keerthishankar")
+
+let name = localStorage.getItem("myName")
+console.log(name)
+
+// localStorage.clear()
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)

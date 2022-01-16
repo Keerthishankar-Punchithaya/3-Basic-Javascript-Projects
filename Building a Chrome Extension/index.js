c4e3b9189a3d149cfe46b73c07a2eb904d055bff
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-16 10:25:45 +0530

    Save the leads to localStorage

--- Building a Chrome Extension/index.js
+++ Building a Chrome Extension/index.js
@@ -1,10 +1,3 @@
-let myLeads = `["www.lead.com"]`
-
-myLeads = JSON.parse(myLeads)
-myLeads.push("www.leads.com")
-myLeads = JSON.stringify(myLeads)
-console.log(typeof myLeads)
-
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -19,7 +12,11 @@ console.log(name)
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+
+    console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads() {

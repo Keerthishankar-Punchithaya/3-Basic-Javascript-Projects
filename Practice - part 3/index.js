6dc033ed554597e7735c69e8c41763a868ef8a2b
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-16 13:43:44 +0530

    Log out items in an array

--- Practice - part 3/index.js
+++ Practice - part 3/index.js
@@ -1,15 +1,9 @@
-const player = "name"
-const opponent = "name2"
-const game = "AmazingFighter"
+let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
 
-let points = 0
-let hasWon = false
-
-points += 100
-hasWon = true
-
-if (hasWon) {
-    console.log(player + " got " + points + " points and won the " + game + " game!")
-} else {
-    console.log(`The winner is ${opponent} ! ${player} lost the ${game} game.`)
+function logItems(arr) {
+    for ( let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
 }
+
+logItems(myCourses)

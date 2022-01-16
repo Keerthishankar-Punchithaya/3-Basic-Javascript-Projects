481f46844759a132f3275581f4f0edd122dbba23
KeerthishankarPunchithaya <keerthishankarp.is19@sahyadri.edu.in>
2022-01-16 12:07:59 +0530

    Arguements vs Parameters

--- Building a Chrome Extension/testing/test.js
+++ Building a Chrome Extension/testing/test.js
@@ -1,3 +1,11 @@
+// const welcomeEl = document.getElementById("welcome-el")
+
+function greetUser(greeting, name, emoji) {
+    welcomeEl.textContent = `${greeting}, ${name}  ${emoji}`
+}
+let hi = "Hello"
+greetUser(hi, "Keerthishankar", "👋")
+
 function add(num1, num2) {
     return num1 + num2 
 }
